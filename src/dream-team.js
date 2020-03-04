c9c420ee9bb9ced521343d65f7546666f5daa2b2
poezhaev.vs <poezhaev.vs@esphere.ru>
2020-03-04 11:33:28 +0300

    feat: solve task dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,16 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+
+  if (!Array.isArray(members)) return false;
+
+  if (members.length === 0) {
+    return false;
+  }
+
+  const letters = members.map(name => {
+    if (typeof name === 'string') {
+      return name.trim()[0].toUpperCase();
+    }
+  });
+
+  return letters.sort().join('');
 };
