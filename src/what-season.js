6ed004092b1a22c213299a3c5ac8cdd6b0ed2d5a
poezhaev.vs <poezhaev.vs@esphere.ru>
2020-03-04 11:37:47 +0300

    solve task what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,37 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if(date === undefined){
+    return "Unable to determine the time of year!"
+  }
+
+  if (!isValidDate(date)) throw Error;
+
+  return getMonthByNumber(date.getMonth());
 };
+
+function getMonthByNumber(num) {
+  switch (num) {
+    case 0:
+    case 1:
+    case 11:
+      return 'winter';
+      break;
+
+    case 2:
+    case 3:
+    case 4:
+      return 'spring';
+      break;
+
+    case 5:
+    case 6:
+    case 7:
+      return 'summer';
+
+    default:
+      return 'autumn';
+  }
+}
+
+function isValidDate(d) {
+  return d instanceof Date && !isNaN(d);
+}
