b8f48bb80a5125a8bf99b3c590854d45ae953eea
poezhaev.vs <poezhaev.vs@esphere.ru>
2020-03-04 11:29:52 +0300

    feat: solve task carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,14 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') return false;
+  const SAMPLE_ACTIVITY = parseFloat(sampleActivity);
+  if (SAMPLE_ACTIVITY <= 0             ||
+    SAMPLE_ACTIVITY >= MODERN_ACTIVITY ||
+    SAMPLE_ACTIVITY >= 9000            ||
+    isNaN(SAMPLE_ACTIVITY)) return false;
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  const HALF_LIFE = Math.log(MODERN_ACTIVITY / SAMPLE_ACTIVITY);
+  return Math.ceil(HALF_LIFE / k);
 };
